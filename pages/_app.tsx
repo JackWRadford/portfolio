fa95a3fdc302a95f59fe4807f1690072d1a3ad5b
JackR <jack.radford@live.co.uk>
2022-10-13 12:53:06 +0100

    Added Head meta data and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "../context/ThemeContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="var(--backgroundColor)" />
+        <meta
+          name="description"
+          content="Portfolio of some of my personal projects."
+        />
+        <title>Jack Radford</title>
+      </Head>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
